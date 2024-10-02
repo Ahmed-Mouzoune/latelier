fa19fc4cc5a3d35f493c406647edce292038d22d
unknown <ahmed.mouzoune92@gmail.com>
2024-10-02 16:15:13 +0200

    style: :art: Fix width header / add wordings on list view & versus view

--- src/app/(versus)/versus/layout.tsx
+++ src/app/(versus)/versus/layout.tsx
@@ -1,6 +1,7 @@
 import CtaList from "@/components/cat/CtaList";
 import Header from "@/components/layout/Header";
 import { cn } from "@/lib/utils";
+import { FaBoltLightning } from "react-icons/fa6";
 
 export default function Layout({
   children,
@@ -10,6 +11,10 @@ export default function Layout({
   return (
     <div className={cn("relative")}>
       <Header />
+      <h2 className="text-primary text-xl sm:text-xl my-2 flex justify-center gap-2 mx-auto">
+        Vote for cutest cat
+        <FaBoltLightning className="fill-yellow-500" />
+      </h2>
       {children}
       <div className="fixed bottom-6 sm:bottom-12 left-1/2 -translate-x-1/2">
         <CtaList />
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="w-screen flex flex-col items-center py-2 bg-transparent">
+    <header className="w-full flex flex-col items-center py-2 bg-transparent">
       <figure className="w-32 sm:w-60 h-auto">
         <Image
           src="/images/catmash-logo.webp"
